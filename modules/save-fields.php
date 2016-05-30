add6a109d91d110c89c0d9a15bae168d958fce85
miziomon <maurizio@mavida.com>
2016-05-30 16:37:23 +0200

    revert

--- modules/save-fields.php
+++ modules/save-fields.php
@@ -9,18 +9,14 @@
 // store the custom field data in the payment meta
 add_filter('edd_payment_meta', function ($payment_meta) {
 
-    // sanitize string 
-    // http://codex.wordpress.org/Validating_Sanitizing_and_Escaping_User_Data
-    $vat = sanitize_text_field($_POST['edd_vat']);
-    $fc = sanitize_text_field($_POST['edd_fc']);
-    $address = sanitize_text_field($_POST['edd_address']);
-    $company = sanitize_text_field($_POST['edd_company']);
+    // sanitize string - http://codex.wordpress.org/Validating_Sanitizing_and_Escaping_User_Data
 
-    //add meta
-    $payment_meta['vat'] = isset($vat) ? $vat : '';
-    $payment_meta['fc'] = isset($fc) ? $fc : '';
-    $payment_meta['address'] = isset($address) ? $address : '';
-    $payment_meta['company'] = isset($company) ? $company : '';
-
+    $payment_meta['vat'] = isset( $_POST['edd_vat'] ) ? sanitize_text_field( $_POST['edd_vat'] ) : '';
+    $payment_meta['fc'] = isset( $_POST['edd_fc'] ) ? sanitize_text_field( $_POST['edd_fc'] ) : '';
+    $payment_meta['address'] = isset( $_POST['edd_address'] ) ? sanitize_text_field( $_POST['edd_address'] ) : '';
+    $payment_meta['company'] = isset( $_POST['edd_company'] ) ? sanitize_text_field( $_POST['edd_company'] ) : '';
+    
     return $payment_meta;
+    
+    
 });
