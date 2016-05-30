b0a0e2535c54e1f67e0fdebfe5f282616b3a0699
miziomon <maurizio@mavida.com>
2016-05-30 15:37:08 +0200

    add localizzation

--- modules/validate-fields.php
+++ modules/validate-fields.php
@@ -10,21 +10,21 @@ add_action('edd_checkout_error_checks', function ( $valid_data, $data ) {
     }
     
     if (empty($data['edd_address'])) {
-        edd_set_error('invalid_address', 'Please enter valid Address.', 'edd_cif');
+        edd_set_error('invalid_address', __('Please enter valid Address.', 'edd_cif'));
     }
     
     if (empty($data['edd_company'])) {
-        edd_set_error('invalid_company', 'Please enter valid Company Name.', 'edd_cif');
+        edd_set_error('invalid_company', __('Please enter valid Company Name.', 'edd_cif'));
     } 
     
     if (empty($data['edd_fc']) ) {
-        edd_set_error('invalid_fc', 'Please enter valid Fiscal code.', 'edd_cif');
+        edd_set_error('invalid_fc', __('Please enter valid Fiscal code.', 'edd_cif'));
     } 
 
     
     $vat_validation = new VatValidator(  $data['edd_vat'] );
     if ( !$vat_validation ) { 
-        edd_set_error('invalid_vat', 'Please enter valid VAT', 'edd_cif');
+        edd_set_error('invalid_vat', __('Please enter valid VAT', 'edd_cif'));
     }
     
   
